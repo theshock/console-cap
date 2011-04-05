c658006f0e14387e7b44e70293b911826e18886d
Shock <shocksilien@gmail.com>
2011-04-05 11:06:52 +0300

    source

---
+++ console.js
@@ -0,0 +1,48 @@
+(function () {
+   var global   = this;
+   var original = global.console;
+   var console  = global.console = {};
+   console.production = false;
+   
+   if (original && !original.time) {
+      original.time = function(name, reset){
+         if (!name) return;
+         var time = new Date().getTime();
+         if (!console.timeCounters) console.timeCounters = {};
+         
+         var key = "KEY" + name.toString();
+         if(!reset && console.timeCounters[key]) return;
+         console.timeCounters[key] = time;
+      };
+
+      original.timeEnd = function(name){
+         var time = new Date().getTime();
+            
+         if(!console.timeCounters) return;
+         
+         var key  = "KEY" + name.toString();
+         var timeCounter = console.timeCounters[key];
+         
+         if (timeCounter) {
+            var diff = time - timeCounter;
+            var label = name + ": " + diff + "ms";
+            console.info(label);
+            delete console.timeCounters[key];
+         }
+         return diff;
+      };
+   }
+   
+   var methods = ['assert', 'count', 'debug', 'dir', 'dirxml', 'error', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd', 'trace', 'warn'];
+   
+   for (var i = methods.length; i--;) {
+      (function (methodName) {
+         console[methodName] = function () {
+            if (original && methodName in original && !console.production) {
+               original[methodName].apply(original, arguments);
+            }
+         };
+      })(methods[i]);
+   }
+})();
+ 
