3033ee6ccfaec2db0611390cc33ba79a24f0a07f
Shock <shocksilien@gmail.com>
2011-04-19 02:51:00 +0300

    firebug is getter, not property. fixed

--- console.js
+++ console.js
@@ -36,18 +36,16 @@
 	}
 
 	var methods = ['assert', 'count', 'debug', 'dir', 'dirxml', 'error', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd', 'trace', 'warn'];
+	var apply   = Function.prototype.apply;
 	
 	for (var i = methods.length; i--;) {
 		(function (methodName) {
 			console[methodName] = function () {
 				if (original && (methodName in original) && !console.production) {
-					var method = original[methodName];
-					// IE 8,9 bug
-					if (typeof method == 'object') {
-						method(arguments);
-					} else {
-						method.apply(original, arguments);
-					}
+					/* we can change this with original[methodName].apply(original, arguments),
+					 * but IE8 & IE9 fails in that case
+					 */
+					apply.call(original[methodName], original, arguments);
 				}
 			};
 		})(methods[i]);
