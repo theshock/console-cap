bf3dcaf3f892d5de088938bb286c26a68df27344
Shock <shocksilien@gmail.com>
2011-04-19 02:21:53 +0300

    ie8,9 bug fixed (console.log is object, not function)

--- console.js
+++ console.js
@@ -1,48 +1,56 @@
 (function () {
-   var global   = this;
-   var original = global.console;
-   var console  = global.console = {};
-   console.production = false;
-   
-   if (original && !original.time) {
-      original.time = function(name, reset){
-         if (!name) return;
-         var time = new Date().getTime();
-         if (!console.timeCounters) console.timeCounters = {};
-         
-         var key = "KEY" + name.toString();
-         if(!reset && console.timeCounters[key]) return;
-         console.timeCounters[key] = time;
-      };
+	var global   = this;
+	var original = global.console;
+	// firebug console has only getter, so we should delete it first
+	delete global.console;
+	var console  = global.console = {};
+	console.production = false;
 
-      original.timeEnd = function(name){
-         var time = new Date().getTime();
-            
-         if(!console.timeCounters) return;
-         
-         var key  = "KEY" + name.toString();
-         var timeCounter = console.timeCounters[key];
-         
-         if (timeCounter) {
-            var diff = time - timeCounter;
-            var label = name + ": " + diff + "ms";
-            console.info(label);
-            delete console.timeCounters[key];
-         }
-         return diff;
-      };
-   }
-   
-   var methods = ['assert', 'count', 'debug', 'dir', 'dirxml', 'error', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd', 'trace', 'warn'];
-   
-   for (var i = methods.length; i--;) {
-      (function (methodName) {
-         console[methodName] = function () {
-            if (original && methodName in original && !console.production) {
-               original[methodName].apply(original, arguments);
-            }
-         };
-      })(methods[i]);
-   }
+	if (original && !original.time) {
+		original.time = function(name, reset){
+			if (!name) return;
+			var time = new Date().getTime();
+			if (!console.timeCounters) console.timeCounters = {};
+			
+			var key = "KEY" + name.toString();
+			if(!reset && console.timeCounters[key]) return;
+			console.timeCounters[key] = time;
+		};
+
+		original.timeEnd = function(name){
+			var time = new Date().getTime();
+				
+			if(!console.timeCounters) return;
+			
+			var key  = "KEY" + name.toString();
+			var timeCounter = console.timeCounters[key];
+			
+			if (timeCounter) {
+				var diff = time - timeCounter;
+				var label = name + ": " + diff + "ms";
+				console.info(label);
+				delete console.timeCounters[key];
+			}
+			return diff;
+		};
+	}
+
+	var methods = ['assert', 'count', 'debug', 'dir', 'dirxml', 'error', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd', 'trace', 'warn'];
+	
+	for (var i = methods.length; i--;) {
+		(function (methodName) {
+			console[methodName] = function () {
+				if (original && (methodName in original) && !console.production) {
+					var method = original[methodName];
+					// IE 8,9 bug
+					if (typeof method == 'object') {
+						method(arguments);
+					} else {
+						method.apply(original, arguments);
+					}
+				}
+			};
+		})(methods[i]);
+	}
 })();
- 
+
